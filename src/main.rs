6e1cadfd0798f27cf57b4d53f65a8dfc75b57079
Hung-Han (Henry) Chen <chenhungh@gmail.com>
2023-11-26 11:00:36 +0200

    Fix typo
    Signed-off-by: Hung-Han (Henry) Chen <chenhungh@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use oxpilot::llm::LLMBuilder;
 async fn main() {
     let llm_builder = LLMBuilder::new()
         .tokenizer_repo_id("hf-internal-testing/llama-tokenizer")
-        .model_repo_id("TheBloke/CodeLlama-7B-GGU")
+        .model_repo_id("TheBloke/CodeLlama-7B-GGUF")
         .model_file_name("codellama-7b.Q2_K.gguf");
-    let _ = llm_builder.build().await;
+    let _ = llm_builder.build().await.expect("Failed to build LLM");
 }
