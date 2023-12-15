036f0ee09425d84ab4fe08509b3d1c2a4673fe0b
Hung-Han (Henry) Chen <chenhungh@gmail.com>
2023-12-15 03:30:42 +0200

    refactored "process" function, added `eos_token` argument, introduced variables for `sampled` and `max_sampled`, removed hardcoded LLM model limit, and updated sampled text sending to the responder.

--- src/process.rs
+++ src/process.rs
@@ -2,43 +2,8 @@ use crate::llm::LLM;
 use crate::token::token_to_text;
 use candle_core::{Device, Tensor};
 use candle_transformers::generation::LogitsProcessor;
-use candle_transformers::models::quantized_llama::MAX_SEQ_LEN;
 
 /// A function that takes a prompt and returns the generated text to a responder.
-/// ```no_run
-/// use tokio::sync::mpsc;
-/// use oxpilot::process::process;
-///  
-/// let mut llm = llm_builder.build().await.expect("Failed to build LLM");
-/// let (sender, mut receiver) = mpsc::channel(32);
-/// let manager = tokio::spawn(async move {
-///   while let Some(cmd) = receiver.recv().await {
-///     let seed = 299792458;
-///     let temperature: f64 = 1.0;
-///     let top_p = 1.1;
-///     let n = 10;
-///     let repeat_last_n = 64;
-///     let repeat_penalty = 1.1;
-///     match cmd {
-///         cmd::Command::Prompt { prompt, responder } => {
-///             process(
-///               prompt,
-///               &mut llm,
-///               responder,
-///               n,
-///               seed,
-///               temperature,
-///               top_p,
-///               repeat_last_n,
-///               repeat_penalty,
-///          ).await;
-///       }
-///     }
-///   }
-/// });
-///
-/// ```
-///
 pub async fn process(
     prompt: String,
     llm: &mut LLM,
@@ -49,21 +14,14 @@ pub async fn process(
     top_p: Option<f64>,
     repeat_last_n: usize,
     repeat_penalty: f32,
+    eos_token: String,
+    max_sampled: usize,
 ) {
-    let tokens = match llm.tokenizer.encode(prompt, true) {
-        Ok(tokens) => tokens,
-        Err(_) => {
-            todo!();
-        }
-    };
-    let pre_prompt_tokens = vec![];
-    let prompt_tokens = [&pre_prompt_tokens, tokens.get_ids()].concat();
-    let prompt_tokens = if prompt_tokens.len() + to_sample > MAX_SEQ_LEN - 10 {
-        let to_remove = prompt_tokens.len() + to_sample + 10 - MAX_SEQ_LEN;
-        prompt_tokens[prompt_tokens.len().saturating_sub(to_remove)..].to_vec()
-    } else {
-        prompt_tokens
-    };
+    let tokens = llm
+        .tokenizer
+        .encode(prompt, true)
+        .expect("Failed to encode prompt as tokens.");
+    let prompt_tokens = tokens.get_ids().to_vec();
     let mut all_tokens: Vec<u32> = vec![];
     let mut logits_processor = LogitsProcessor::new(seed, Some(temperature), top_p);
     let mut next_token = {
@@ -76,6 +34,14 @@ pub async fn process(
         logits_processor.sample(&logits).unwrap()
     };
     all_tokens.push(next_token);
+    responder
+        .send((token_to_text(next_token, &llm.tokenizer)).to_string())
+        .await
+        .unwrap();
+
+    let eos_token_id = *llm.tokenizer.get_vocab(true).get(&eos_token).unwrap();
+
+    let mut sampled = 0;
     for index in 0..to_sample {
         let input = Tensor::new(&[next_token], &Device::Cpu)
             .unwrap()
@@ -93,6 +59,16 @@ pub async fn process(
             &all_tokens[start_at..],
         );
         next_token = logits_processor.sample(&logits).unwrap();
+        sampled += 1;
+        if next_token == 32000 {
+            break;
+        };
+        if sampled >= max_sampled {
+            break;
+        }
+        if next_token == eos_token_id {
+            break;
+        }
         all_tokens.push(next_token);
         let text = token_to_text(next_token, &llm.tokenizer);
         responder.send((text).to_string()).await.unwrap();
