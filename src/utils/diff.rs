0e958885056cce823c9fc5068666a55ae4505fbd
Hung-Han (Henry) Chen <chenhungh@gmail.com>
2023-12-16 12:11:28 +0200

    feat: implement get\_diff function with '--function-context' option for Git, supporting staged files and handling diff output. Previously used 'git diff --function-context' command directly.

--- src/utils/diff.rs
+++ src/utils/diff.rs
@@ -1,19 +1,22 @@
 use tracing::warn;
 
 /// Get the diff of the staged files. None if there is no diff/failed to get the diff.
-pub fn get_diff() -> Option<String> {
-    let stagged_diff = std::process::Command::new("git")
-        .arg("diff")
+pub fn get_diff(function_context: bool) -> Option<String> {
+    let mut git = std::process::Command::new("git");
+    git.arg("diff")
         .arg("--staged")
         .arg("--ignore-all-space")
-        .arg("--diff-algorithm=minimal")
-        .arg("--function-context")
+        .arg("--ignore-blank-lines")
+        .arg("--diff-algorithm=histogram")
         .arg("--no-ext-diff")
-        .arg("--no-color")
-        .output()
-        .expect("failed to execute diff");
-    if stagged_diff.status.success() {
-        match String::from_utf8(stagged_diff.stdout) {
+        .arg("--no-color");
+
+    if function_context {
+        git.arg("--function-context");
+    }
+    let output = git.output().expect("failed to execute diff");
+    if output.status.success() {
+        match String::from_utf8(output.stdout) {
             Ok(stdout) => {
                 if stdout.len() > 0 {
                     return Some(stdout);
